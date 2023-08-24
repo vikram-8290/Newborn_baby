03fab6b99f2bae661b0d5dbfca76500dfd02596a
vikram-8290 <113750425+vikram-8290@users.noreply.github.com>
2023-08-24 10:23:39 +0530

    New Feature is to Calculate the percentage of Perdiction

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pickle
 
 app=Flask(__name__)
+dataset = pd.read_csv('newborn.csv')  
 
 regmodel = pickle.load(open('regmodel.pkl', 'rb'))
 
@@ -17,6 +18,20 @@ def predict_api():
     data = request.json['data']
     output = regmodel.predict([np.array(list(data.values()))])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    k = '98.2%'
+    data = [float(x) for x in request.form.values()]
+    output = regmodel.predict([np.array(data)])
+    original_hemoglobin = data[2]  # Assuming original hemoglobin is at index 2
+    post_transfusion_hemoglobin = output[0]  # Assuming prediction is post-transfusion hemoglobin
+    #Handling The Case Of Original Hemoglobin Level Is Too Low To Calculate Percentage Change
+    if original_hemoglobin > 0.01:  # Adjust the threshold as needed
+        prediction_percentage = abs((post_transfusion_hemoglobin - original_hemoglobin) / original_hemoglobin) * 100
+        return render_template('index.html', prediction_text='Predicted Value is {} Predicted percentage  is {:.2f}% '.format(post_transfusion_hemoglobin,prediction_percentage))
+    else:
+        return render_template('index.html', prediction_text='Predicted Value is {} Predicted percentage is {} '.format(post_transfusion_hemoglobin, k))
 
 if __name__ == "__main__":
     app.run(debug=True) 
