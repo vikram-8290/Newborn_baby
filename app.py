8335646781aa00cfa0ab82446b814a5790115f03
vikram-8290 <113750425+vikram-8290@users.noreply.github.com>
2023-11-07 16:59:13 +0530

    Fix waring In Juypter Notebook

--- app.py
+++ app.py
@@ -24,11 +24,13 @@ def predict():
     k = 98.2
     data = [float(x) for x in request.form.values()]
     output = regmodel.predict([np.array(data)])
-    original_hemoglobin = data[2]  # Assuming original hemoglobin is at index 2
-    post_transfusion_hemoglobin = output[0]  # Assuming prediction is post-transfusion hemoglobin
+    original_hemoglobin = data[2]
+    post_transfusion_hemoglobin = output[0]
+     # Assuming prediction is post-transfusion hemoglobin
     #Handling The Case Of Original Hemoglobin Level Is Too Low To Calculate Percentage Change
     if original_hemoglobin > 0.01:  # Adjust the threshold as needed
         prediction_percentage = abs((post_transfusion_hemoglobin - original_hemoglobin) / original_hemoglobin) * 100
+        prediction_percentage = min(prediction_percentage, 98.2)
         return render_template('index.html', prediction_text='Predicted Value of Post-Hemoglobin is {:.2f}\nPredicted percentage of Post-Hemoglobin is {:.2f}% '.format(post_transfusion_hemoglobin,prediction_percentage))
     else:
         return render_template('index.html', prediction_text='Predicted Value of Post-Hemoglobin is  {:.2f}\nPredicted percentage of Post-Hemoglobin is {:.2f}% '.format(post_transfusion_hemoglobin, k))
